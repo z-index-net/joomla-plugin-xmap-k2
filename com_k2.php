f7e13c49047f3df85a595ce23cda2e87429a91cb
b2un0 <branko.wilhelm@gmail.com>
2015-02-25 21:04:50 +0100

    fix: joomla language standards

--- com_k2.php
+++ com_k2.php
@@ -3,25 +3,23 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 - 2014 Branko Wilhelm
+ * @copyright  (c) 2013 - 2015 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 defined('_JEXEC') or die;
 
-final class xmap_com_k2
+class xmap_com_k2
 {
-
     private static $layouts = array('category', 'tag', 'user');
+
     private static $enabled = false;
 
     public function __construct()
     {
         self::$enabled = JComponentHelper::isEnabled('com_k2');
 
-        if (self::$enabled) {
-            require_once JPATH_SITE . '/components/com_k2/helpers/route.php';
-        }
+        JLoader::register('K2HelperRoute', JPATH_SITE . '/components/com_k2/helpers/route.php');
     }
 
     public static function getTree(XmapDisplayer &$xmap, stdClass &$parent, array &$params)
